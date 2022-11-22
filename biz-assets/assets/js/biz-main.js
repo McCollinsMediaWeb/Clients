130a9a3f3f3383e7ace215eed0359b0f77cf0ab5
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2022-11-22 23:13:18 +0530

    ijas

--- biz-assets/assets/js/biz-main.js
+++ biz-assets/assets/js/biz-main.js
@@ -175,6 +175,19 @@ jQuery(document).ready(function ($) {
       }
     ]
   });
+  jQuery(".MobSlider").slick({
+    draggable: true,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    arrows: false,
+    dots: true,
+    fade: true,
+    speed: 500,
+    infinite: true,
+    cssEase: 'ease-in-out',
+    touchThreshold: 100
+  });
+  
 
 
   
